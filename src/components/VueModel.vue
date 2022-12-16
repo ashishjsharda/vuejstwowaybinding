e8b1d01450a81b8c5acfd6f585076c9999d643ea
Ashish Sharda <ashishjsharda@gmail.com>
2022-12-16 06:33:55 -0600

    update vuemodel

--- src/components/VueModel.vue
+++ src/components/VueModel.vue
@@ -1,23 +1,38 @@
 <template>
-  <div class="form">
-    <label>
-      <span>First Name</span>
-      <input type="text" v-model="name" />
-    </label>
-  </div>
+  <section>
+    <div class="form">
+      <label>
+        Name
+        <input type="text" v-model="name" />
+      </label>
+      <label>
+        Preferred javascript style
+        <select name="language" v-model="language">
+          <option value="Javascript">JavaScript</option>
+          <option value="TypeScript">TypeScript</option>
+          <option value="CoffeeScript">CoffeeScript</option>
+
+        </select>
+      </label>
+    </div>
+    <ul class="overview">
+      <li><strong>Overview</strong></li>
+      <li>Name: {{ name }}</li>
+      <li>Preference: {{ language }}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      name: "John"
-    };
-  }
-
+      name: '',
+      language: '',
+    }
+  },
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
